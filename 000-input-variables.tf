e090d1d667cce37c6ead5eed62d0e74e49c0ac42
Matthieu Chevreux <chevreux.matthieu@hotmail.com>
2020-12-05 14:49:36 +0100

    docs: added general documentation (#4)
    * docs(input-variables): added input variables description
    
    * docs(variables): added variables description
    
    * doc(README): updated README.md with an example

--- 000-input-variables.tf
+++ 000-input-variables.tf
@@ -1,12 +1,22 @@
 variable "router_id" {
   type = string
+  description = "ID of the routeur to link the network to."
 }
 
 variable "network" {
-  type = object({ name=string, subnet_name=string, cidr=string })
+  type = object({
+    name=string,
+    subnet_name=string,
+    cidr=string
+    })
+  description = "An object describing the network to create."
 }
 
 variable "dns_ip" {
   type = list(string)
-  default = ["1.1.1.1", "1.0.0.1"]
+  default = [
+    "1.1.1.1",
+    "1.0.0.1"
+  ]
+  description = "A list of IP Addresses of the DNS to refer to. Default is set to 1.1.1.1 and 1.0.0.1."
 }
